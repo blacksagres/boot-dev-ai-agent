6ab57a1fabb8ef001369a1f69ef24dafe14728a5
blacksagres <geraldo.nogueirabs@gmail.com>
2025-10-31 13:40:33 +0100

    we now accept arguments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 load_dotenv()
@@ -11,27 +12,46 @@ client = genai.Client(api_key=api_key)
 def main():
 
     input = ""
+    is_verbose = False
 
-    # without an input, throw an error
+    
+    # - If no input, we throw.
+    # - Then check for arguments.
     try:
         input = sys.argv[1]
-    except:
+        arguments = sys.argv[2:]
+
+        is_verbose = "--verbose" in arguments
+
+    except IndexError:
+        print('Error processing the input and/or arguments.')
         return sys.exit(1)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return sys.exit(1)
+    
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=input)])
+    ]
     
     print("Hello from boot-dev-ai-agent!")
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=input
+        contents=messages
     )
 
     print(response.text)
-    print("----- USAGE ------")
 
-    print('Prompt tokens:', response.usage_metadata.prompt_token_count)
-    print('Response tokens:', response.usage_metadata.candidates_token_count)
 
-    print("------------------")
+    if is_verbose:
+        print("----- USAGE ------")
+
+        print('User prompt:', input)
+        print('Prompt tokens:', response.usage_metadata.prompt_token_count)
+        print('Response tokens:', response.usage_metadata.candidates_token_count)
+
+        print("------------------")
 
 
 
