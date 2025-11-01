888aa6a12c870e9138c7625615c93521d200bd11
blacksagres <geraldo.nogueirabs@gmail.com>
2025-11-01 09:57:20 +0100

    make bot more powerful

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ You are a helpful AI coding agent.
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
 - List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
@@ -61,9 +64,57 @@ def main():
         ),
     )
 
+    schema_get_file_content = types.FunctionDeclaration(
+        # our harcoded function
+        name="get_file_content",
+        description="Gets he full content of files in the specified folder, constrained to the working directory.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="The directory to get files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                ),
+            },
+        ),
+    )
+
+    schema_run_python_file = types.FunctionDeclaration(
+        # our harcoded function
+        name="run_python_file",
+        description="Writes python scripts, constrained to the working directory.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="The directory to run files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                ),
+            },
+        ),
+    )
+
+    schema_write_file = types.FunctionDeclaration(
+        # our harcoded function
+        name="write_file",
+        description="Writes content to files, constrained to the working directory.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="The directory to write files to, relative to the working directory. If not provided, lists files in the working directory itself.",
+                ),
+            },
+        ),
+    )              
+
     available_functions = types.Tool(
         function_declarations=[
             schema_get_files_info,
+            schema_get_file_content,
+            schema_run_python_file,
+            schema_write_file
         ]
     )
 
